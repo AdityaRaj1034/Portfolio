ca81142e01c7c829607d732e9a544256fee1916d
AdityaRaj1034 <143100702+AdityaRaj1034@users.noreply.github.com>
2025-04-30 01:25:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active')
+}
+
+
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrolly;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+/*===== ===== sticky navbar ===*/
+let header = document.querySelector('header');
+header.classList.toggle('sticky', window.scrollY > 100);
+
+
+
+menuIcon.classList.remove('fa-xmark');
+navbar.classList.remove('active');
+};
+
+
+/*========= scroll reveal ============*/
+
+ScrollReveal({
+distance: '80px',
+duration: 2000,
+delay: 200,
+});
+ScrollReveal().reveal ('.home-content, heading', { origin: 'top' });
+ScrollReveal().reveal ('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'buttom' });
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+
+
+
+
+/*======= ======== typed js ========= ===*/
+const typed = new Typed('.multiple-text', {
+    strings: ["I'm a Web Developer","I'm a Java/Python Coder"," I'm currently working as Operations Executive, Level 2 at Uber Process ",' I was Campus Ambasssador for IIM Banglore Vista 2023 (The International Summit for Young Leaders).',' I also Volunteered for Swapna Foundation, Ngo.'],
+    typeSpeed: 40,
+    backSpeed: 10,
+    backDelay: 1000,
+    loop: true,
+    });          
